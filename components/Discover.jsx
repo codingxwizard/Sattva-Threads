21dd5fa51ca0deb102e97154c92f1c0d7287df2c
Raja Singh <rajasgh18@gmail.com>
2023-10-07 17:29:57 +0530

    linked navbar buttons

--- components/Discover.jsx
+++ components/Discover.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react';
 
 export default function Discover() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const containerRef = useRef(null);
+  
+    const scrollToCenter = () => {
+      if (containerRef.current) {
+        const containerWidth = containerRef.current.offsetWidth;
+        const itemWidth = containerWidth / 4;
+        const centerIndex = Math.floor(4 / 2);
+        const scrollX =
+          centerIndex * itemWidth - containerWidth / 2 + itemWidth / 2;
+        containerRef.current.scroll({
+          left: scrollX,
+          behavior: 'smooth',
+        });
+      }
+    };
+  
+    useEffect(() => {
+      scrollToCenter();
+    }, [currentIndex]);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === 4 - 1 ? 0 : prevIndex + 1
+        );
+      }, 3000);
+  
+      return () => {
+        clearInterval(interval);
+      };
+    }, [3000]);
+  
+    const handleItemClick = (index) => {
+      setCurrentIndex(index);
+    };
+
     return (
         <section className='w-full flex flex-col gap-10 items-center p-10 pb-16 font-light bg-[#EFEFEF] text-primary'>
             <h1>Discover More</h1>
-            <section className='flex h-96 items-center overflow-x-auto gap-10'>
-                <img src="discover1.png" className='h-3/4 rounded' alt="" />
-                <img src="discover2.png" className='h-3/4 rounded' alt="" />
-                <img src="collection2.png" className='h-full rounded' alt="" />
-                <img src="collection4.png" className='h-3/4 rounded' alt="" />
+            <section ref={containerRef} className='flex items-center overflow-x-auto gap-10'>
+                <img onClick={() => handleItemClick(0)} src="discover1.png" className={`rounded w-3/4 ${0 === currentIndex ? 'scale-125' : 'scale-100'}`} alt="" />
+                <img onClick={() => handleItemClick(1)} src="discover2.png" className={`rounded w-3/4 ${1 === currentIndex ? 'scale-125' : 'scale-100'}`} alt="" />
+                <img onClick={() => handleItemClick(2)} src="collection2.png" className={`rounded w-3/4 ${2 === currentIndex ? 'scale-125' : 'scale-100'}`} alt="" />
+                <img onClick={() => handleItemClick(3)} src="collection4.png" className={`rounded w-3/4 ${3 === currentIndex ? 'scale-125' : 'scale-100'}`} alt="" />
             </section>
         </section>
     )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,30 +2,28 @@ import Link from 'next/link'
 import React from 'react'
 import { PiUserLight } from 'react-icons/pi'
 import { BsSearch, BsBag } from 'react-icons/bs'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 export default function Navbar() {
     const router = useRouter();
     const isHome = router.pathname === '/';
-    console.log(isHome)
     return (
         <nav className={`h-40 z-10 justify-evenly border-b ${isHome ? "text-white border-white" : "text-primary border-primary"} p-2 ${isHome && "absolute"} w-full flex flex-col`}>
             <img onClick={()=> router.push('/')} src={isHome ? '/LogoWhite.svg' : '/LogoRed.svg'} className='cursor-pointer h-20 mx-auto' alt='Sattva Threads' />
             <section className='flex justify-evenly'>
                 <div></div>
                 <div className='flex gap-20'>
-                    <Link href="">New Arrivals</Link>
-                    <Link href="">Saree</Link>
-                    <Link href="">Kurta</Link>
+                    <Link href="/new-arrivals">New Arrivals</Link>
+                    <Link href="/saree">Saree</Link>
+                    <Link href="/kurta">Kurta</Link>
                     <Link href="">Salwar & Suit</Link>
-                    <Link href="">Sale</Link>
-                    <Link href="">Customer Support</Link>
+                    <Link href="/sale">Sale</Link>
+                    <Link href="/customer-support">Customer Support</Link>
                 </div>
                 <div className='flex gap-10 items-center'>
-                    <Link href=""><PiUserLight className='h-6 w-6' /></Link>
+                    <Link href="/login"><PiUserLight className='h-6 w-6' /></Link>
                     <Link href=""><BsSearch className='h-5 w-5' /></Link>
-                    <Link href=""><BsBag className='h-5 w-5' /></Link>
+                    <Link href="/cart"><BsBag className='h-5 w-5' /></Link>
                 </div>
             </section>
         </nav>
