5d3adfe650d80ea41d468c8072a8eecda7037174
Raja Singh <rajasgh18@gmail.com>
2023-10-31 16:51:28 +0530

    Fixed Bug

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,13 @@ import { SessionProvider } from "next-auth/react"
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
 
   return <>
-    <SessionProvider session={session}>
-      <UserState>
-        <Head>
-          <title>Sattva Threads</title>
-        </Head>
-        <Component {...pageProps} />
-      </UserState>
-    </SessionProvider>
+    {/* <SessionProvider session={session}> */}
+    <UserState>
+      <Head>
+        <title>Sattva Threads</title>
+      </Head>
+      <Component {...pageProps} />
+    </UserState>
+    {/* </SessionProvider> */}
   </>
 }
